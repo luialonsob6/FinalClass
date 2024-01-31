e125095f4c9e680f9238d91980f580e6b79084e8
luialonsob6 <luis.alonso6@alumni.esade.edu>
2024-01-31 12:01:31 +0100

    Data Analysis script

---
+++ scripts/Data_analysis.py
@@ -0,0 +1,58 @@
+"""
+This script will be used to analyse the dataframe
+"""
+import unittest
+import pandas as pd
+
+class Dataanalysis:
+
+    def __init__(self, df):
+        self.df = df
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
