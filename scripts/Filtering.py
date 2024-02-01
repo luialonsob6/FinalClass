8bc60aa49cffba6bc4d325b8fe1ed0fcde1e14d6
luialonsob6 <luis.alonso6@alumni.esade.edu>
2024-02-01 11:45:59 +0100

    Filtering script finished

--- scripts/Filtering.py
+++ scripts/Filtering.py
@@ -2,8 +2,10 @@
 Script to make filter of the dataset
 """
 import unittest
+import os
 import pandas as pd
 import click
+
 
 
 class Filterdata:
@@ -20,11 +22,11 @@ class Filterdata:
         """
         return self.df[self.df["neighbourhood_group"] == neighbourhood]
 
-    def filter_type(self, type):
+    def filter_type(self, room):
         """
         Filter by room type
         """
-        return self.df[self.df["Year"] == type]
+        return self.df[self.df["Year"] == room]
 
     def filter_higher_mean(self):
         """
@@ -32,28 +34,29 @@ class Filterdata:
         """
         mean = self.df["price"].mean()
         return self.df[self.df["price"] > mean]
-    
-    def filter_min_nights(self,nights):
+
+    def filter_min_nights(self, nights):
         """
         Filter by minimum nights
         """
         return self.df[self.df["minimum_nights"] < nights]
-    
+
     def filter_availability(self, availability):
         """
         Filter by the days available
         """
-        return self.df[self.df["availability_365"]>= availability ]
+        return self.df[self.df["availability_365"] >= availability]
 
 
 @click.command(short_help="parser to import dataset")
 @click.option("-i", "--insert", required=True, help="Path to my Input Dataset")
+@click.option("-o", "--output", required=True, help="Where to store the filtered dataset")
 @click.option("-f", "--filtering", is_flag=True, help="Set a filtering or not")
-@click.option("-n", "--neighbourhood", required=True, help="Neighbourhood to filter")
-@click.option("-t", "--type", required=True, help="Room type to filter")
-@click.option("-ni", "--nights", required=True, help="Minimum nights to filter")
-@click.option("-a", "--availability", required=True, help="Availability to filter")
-def main(insert, filtering, type, neighbourhood, nights, availability):
+@click.option("-n", "--neighbourhood", required=True, help="Neighbour filter")
+@click.option("-r", "--room", required=True, help="Room type to filter")
+@click.option("-ni", "--nights", required=True, help="Minimum nights")
+@click.option("-a", "--availability", required=True, help="Available filter")
+def main(insert, output, filtering, room, neighbourhood, nights, availability):
     """
     Start functions
     """
@@ -66,14 +69,18 @@ def main(insert, filtering, type, neighbourhood, nights, availability):
     if filtering:
         print("Im going to filter")
         df = Filterdata(df).filter_neighbourhood(neighbourhood)
-        df = Filterdata(df).filter_type(type)
+        df = Filterdata(df).filter_type(room)
         df = Filterdata(df).filter_higher_mean()
         df = Filterdata(df).filter_min_nights(nights)
         df = Filterdata(df).filter_availability(availability)
 
-
         print(df.shape)
         print(df.head())
+
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    df.to_csv(f"{output}/filtered_df.csv", index=None)
 
 
 if __name__ == "__main__":
